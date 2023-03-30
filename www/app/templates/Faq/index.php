6cca9e8827641466e66196e0b780738e321381f8
ForcePalm <48830341+ForcePalm@users.noreply.github.com>
2023-03-30 09:31:15 +0200

    no changes

--- www/app/templates/Faq/index.php
+++ www/app/templates/Faq/index.php
@@ -4,25 +4,21 @@
  * @var iterable<\App\Model\Entity\Faq> $faq
  */
 ?>
-<div class="col options-header"></div>
-    <a href="<?= $this->Url->build(['controller' => 'Faq', 'action' => 'add']) ?> ">Add</a>                
-</div>
 <div class="accordion accordion-wrapper" id="faq">
-<h3>FAQ</h3>
-<?php foreach ($faq as $faq): ?>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="heading-<?= $faq->id ?>">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= $faq->id ?>" aria-expanded="false" aria-controls="collapse-<?= $faq->id ?>">
-      <?= $faq->title ?>
-      </button>
-    </h2>
-    <div id="collapse-<?= $faq->id ?>" class="accordion-collapse collapse" aria-labelledby="heading-<?= $faq->id ?>" data-bs-parent="#faq">
-      <div class="accordion-body">
-      <?=$faq->content ?>
+  <h3>FAQ</h3>
+  <?php foreach ($faq as $faq): ?>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="heading-<?= $faq->id ?>">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= $faq->id ?>" aria-expanded="false" aria-controls="collapse-<?= $faq->id ?>">
+          <?= $faq->title ?>
+        </button>
+      </h2>
+      <div id="collapse-<?= $faq->id ?>" class="accordion-collapse collapse" aria-labelledby="heading-<?= $faq->id ?>" data-bs-parent="#faq">
+        <div class="accordion-body">
+          <?=$faq->content ?> 
+        </div>
       </div>
     </div>
-  </div>
   <?php endforeach; ?>
-
 </div>
 
